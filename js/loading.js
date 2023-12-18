6f5acb889f4d1872e8b4ed91548c00acf84f23e2
Amani-Sena <amani.sena955@gmail.com>
2023-12-18 16:53:34 -0300

    Atualização e melhorias no portfólio

--- js/loading.js
+++ js/loading.js
@@ -1,18 +1,4 @@
 setTimeout( function() {
     const load = document.getElementById("preloader")
     load.classList.add('close-loading')    
-}, 2500);
-
-
-
-
-/*function abrirModal($var) {
-    const modal = document.getElementById($var)
-    modal.classList.toggle('abrir')
-
-    modal.addEventListener("click", (e) => {
-        if(e.target.id == 'fechar' || e.target.id == $var || e.target.id == 'btn-cancel') {
-            modal.classList.remove('abrir')
-        }
-    })
-}*/
+}, 2100);
